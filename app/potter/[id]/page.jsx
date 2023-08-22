e1f2a6bcedb1bd0446b559f7b4b481ad04181b16
wyclif-maitho <isaacmaitho14@gmail.com>
2023-08-22 15:06:51 -0400

    Mapping the Search bar and its functionality

--- app/potter/[id]/page.jsx
+++ app/potter/[id]/page.jsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { FaHandPointLeft } from "react-icons/fa";
+
 async function getCharacterDetails(id) {
     const res = await fetch(`https://hp-api.onrender.com/api/character/${id}`);
 
@@ -8,16 +11,21 @@ export default async function DetailsPage({ params }) {
   const character = await getCharacterDetails(params.id)
   console.log(character)
 
- /* if (!character) {
+ if (!character) {
     return <p>Loading...</p>;
-  }*/
-
+  }
+  
   return (
     <div>
+    <Link href='/potter'>
+      <FaHandPointLeft className='inline pr-2 text-3xl'/>
+      <h2> Back </h2>
+    </Link>
     {character.length > 0 ? (
         character.map(character => (
             <div key={character.id} className='card my-5'>
             <h2 className="text-center">{character.name}</h2>
+            <img src={character.image} height={40} width={70}/>
                 <h3>Other Names: {character.alternate_names}</h3>
                 <h3>Gender: {character.gender}</h3>
                 <h3>House: {character.house}</h3>
